4141ef799d42dcbdf37ebe0872f1e8ad296e8816
沈彬峰 <shen.binfeng@yalla.live>
2023-05-26 11:37:35 +0800

    add 注释

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//主页
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
